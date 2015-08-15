610d8f5e73573d50ae941a4c82deafdfe7c48f67
Laszlo Korte <me@laszlokorte.d>
2015-08-15 21:52:42 +0200

    draw crop handles

--- ReformApplication/ReformApplication/CanvasView.swift
+++ ReformApplication/ReformApplication/CanvasView.swift
@@ -15,6 +15,25 @@ class CanvasView : NSView {
     
     let canvasSize = (300,300)
     
+    private var currentContext : CGContext? {
+        get {
+            // The 10.10 SDK provides a CGContext on NSGraphicsContext, but
+            // that's not available to folks running 10.9, so perform this
+            // violence to get a context via a void*.
+            // iOS can just use UIGraphicsGetCurrentContext.
+            
+            let unsafeContextPointer = NSGraphicsContext.currentContext()?.graphicsPort
+            
+            if let contextPointer = unsafeContextPointer {
+                let opaquePointer = COpaquePointer(contextPointer)
+                let context: CGContextRef = Unmanaged.fromOpaque(opaquePointer).takeUnretainedValue()
+                return context
+            } else {
+                return nil
+            }
+        }
+    }
+    
     override func drawRect(dirtyRect: NSRect) {
         let ownBounds = bounds
         let paperRect = NSRect(x:(ownBounds.width-CGFloat(canvasSize.0))/2.0,y:(ownBounds.height-CGFloat(canvasSize.1))/2.0, width: CGFloat(canvasSize.0), height: CGFloat(canvasSize.1))
@@ -22,6 +41,27 @@ class CanvasView : NSView {
         paperColor.set()
         NSBezierPath.fillRect(paperRect)
         
+        if let context = currentContext {
+            
+            CGContextTranslateCTM(context, 25, 25)
+            CGContextSetRGBFillColor(context, 0.23, 0.85, 0.3, 1)
+            CGContextSetRGBStrokeColor(context, 0.18, 0.5, 0.24, 1)
+            CGContextSetLineWidth(context, 2)
+            
+            let dotSize = 12
+            for x in [-1,0,1] {
+                for y in [-1,0,1] {
+                    guard x != 0 || y != 0 else { continue }
+                    
+                    let center = ( (1+x) * canvasSize.0/2, (1+y) * canvasSize.1/2 )
+                    
+                    let rect = CGRect(x:center.0-dotSize/2, y:center.1-dotSize/2, width: dotSize, height: dotSize)
+                    CGContextFillEllipseInRect(context, rect)
+                    CGContextStrokeEllipseInRect(context, rect)
+                }
+            }
+
+        }
     }
     
     override var intrinsicContentSize : NSSize {
