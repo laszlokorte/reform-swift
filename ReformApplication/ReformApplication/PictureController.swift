3fc01a4754bb90e691a30f3dbc6931ed92a9d997
Laszlo Korte <me@laszlokorte.d>
2015-08-15 19:14:59 +0200

    Clean up storyboard

---
+++ ReformApplication/ReformApplication/PictureController.swift
@@ -0,0 +1,18 @@
+//
+//  PictureController.swift
+//  ReformApplication
+//
+//  Created by Laszlo Korte on 15.08.15.
+//  Copyright © 2015 Laszlo Korte. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class PictureController : NSViewController {
+
+}
+
+extension PictureController : NSSplitViewDelegate {
+
+}
---
+++ ReformApplication/ReformApplication/StageController.swift
@@ -0,0 +1,14 @@
+//
+//  StageController.swift
+//  ReformApplication
+//
+//  Created by Laszlo Korte on 15.08.15.
+//  Copyright © 2015 Laszlo Korte. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class StageController : NSViewController {
+
+}
