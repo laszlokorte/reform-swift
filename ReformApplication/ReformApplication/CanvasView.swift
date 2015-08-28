6413ff906871c97a6a43fb64ec6c07c7bf37131b
Laszlo Korte <me@laszlokorte.d>
2015-08-28 18:37:56 +0200

    fix canvas size

--- ReformApplication/ReformApplication/CanvasView.swift
+++ ReformApplication/ReformApplication/CanvasView.swift
@@ -155,24 +155,3 @@ extension CanvasView {
     
     }
 }
-
-
-class CenteredClipView:NSClipView
-{
-    override func constrainBoundsRect(proposedBounds: NSRect) -> NSRect {
-        
-        var rect = super.constrainBoundsRect(proposedBounds)
-        if let containerView = self.documentView as? NSView {
-            
-            if (rect.size.width > containerView.frame.size.width) {
-                rect.origin.x = (containerView.frame.width - rect.width ) / 2
-            }
-            
-            if(rect.size.height > containerView.frame.size.height) {
-                rect.origin.y = (containerView.frame.height - rect.height ) / 2
-            }
-        }
-        
-        return rect
-    }
-}
