dd349db4c4e489b5ad8fde94a27ec7a15c6370cf
Laszlo Korte <me@laszlokorte.de>
2015-10-28 02:33:09 +0100

    implement json encoding

--- ReformSerializer/ReformSerializer/JsonFormat.swift
+++ ReformSerializer/ReformSerializer/JsonFormat.swift
@@ -10,7 +10,22 @@ final public class JsonFormat : Encoder, Decoder {
     public init() {}
 
     public func encode(value: NormalizedValue) -> String {
-        return "asd"
+        switch value {
+        case .Null:
+            return "null"
+        case .Bool(let b):
+            return b ? "true" : "false"
+        case .String(let s):
+            return "\"\(s)\""
+        case .Int(let i):
+            return String(i)
+        case .Double(let d):
+            return String(format: "%f", d)
+        case .Array(let arr):
+            return String(format: "[%@]", arr.map({ encode($0) }).joinWithSeparator(","))
+        case .Dictionary(let dict):
+            return String(format: "{%@}", dict.map({ (k,v) in return "\"\(k)\":\(encode(v))" }).joinWithSeparator(","))
+        }
     }
 
     public func decode(string: String) -> NormalizedValue? {
